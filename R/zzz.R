ab6ee26dfe1fc41f05014ce965fd7c4f98a05c01
Maarten Behn <maarten.behn@gmail.com>
2025-05-27 16:59:35 +0200

    remove missing google fonts

--- R/zzz.R
+++ R/zzz.R
@@ -15,14 +15,14 @@
 
 .onLoad <- function(libname, pkgname) {
   # Load fonts
-  sysfonts::font_add_google("Caveat")
-  sysfonts::font_add_google("Imbue")
-  sysfonts::font_add_google("Damion")
-  sysfonts::font_add_google("Oswald")
-  sysfonts::font_add_google("Rampart One")
-  sysfonts::font_add_google("Fredericka the Great")
-  sysfonts::font_add_google("Dancing Script")
-  sysfonts::font_add_google("Walter Turncoat")
+  #sysfonts::font_add_google("Caveat")
+  #sysfonts::font_add_google("Imbue")
+  #sysfonts::font_add_google("Damion")
+  #sysfonts::font_add_google("Oswald")
+  #sysfonts::font_add_google("Rampart One")
+  #sysfonts::font_add_google("Fredericka the Great")
+  #sysfonts::font_add_google("Dancing Script")
+  #sysfonts::font_add_google("Walter Turncoat")
   sysfonts::font_add("American Uncial Regular", system.file("fonts", "uncial.otf", package = "rcityviews"))
   sysfonts::font_add("Rage", system.file("fonts", "rage.ttf", package = "rcityviews"))
   # Call showtext_auto
